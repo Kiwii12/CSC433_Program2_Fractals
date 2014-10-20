24619c2cdac991df9971add648a522bda1cf016f
Deaboy <danieleandrus@gmail.com>
2014-10-20 13:53:35 -0600

    Updated Documentation
    - Added project documentation to Main.cpp
    - Updated documentation for Main.cpp
    
    Fixed compile errors in Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,85 @@
+/***************************************************************************//**
+ * @file 
+ *
+ * @mainpage Program 2 - Fractals
+ * 
+ * @section course_section Course Information 
+ *
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @date Oct 24, 2014
+ * 
+ * @par Professor: 
+ *         Dr. Weiss
+ * 
+ * @par Course: 
+ *         CSC-433 - M001-2014FA -  9:00-9:50 AM MWF
+ * 
+ * @par Location: 
+ *         McLaury-311
+ *
+ * @section program_section Program Information 
+ * 
+ * @details This is a fractal drawing program designed to allow a user to
+ * generate complex fractals using a simple graphical interface.
+ *
+ * GLUT and OpenGL are the libraries used for gui management.
+ *
+ * @section compile_section Compiling and Usage
+ *
+ * @par Compiling Instructions: 
+ *      Build and run
+ * 
+ * @par Build:
+   @verbatim
+   > g++ -std=c++11 -lGL -lglut -lGLU -o fractals
+   @endverbatim
+ * 
+ * @par Make:
+   @vertbatim
+   > make fractals
+   @endverbatim
+ * 
+ * @par Usage: 
+   @verbatim  
+   > fractals
+   @endverbatim 
+ *
+ * @section todo_bugs_modification_section Todo, Bugs, and Modifications
+ * 
+ * @par Modifications and Development Timeline: 
+   @verbatim 
+   Date          Modification 
+   ----------   ---------------------------------------------------------------
+   10-05-2014	Created GitHub repo
+   10-18-2014	Added program components and fractal functions
+   @endverbatim
+ *
+ ******************************************************************************/
 
-
-
+/*******************************************************************************
+ *                 DECLARATIONS, INCLUDES, AND NAMESPACES
+*******************************************************************************/
+#include "Fractal.h"
 
 /**************************************************************************//**
- * @author Daniel Andrus
+ * @author Johnny Ackerman
  * 
- * @par Description: 
- * This function primarly passes control to the Fractol class.
+ * @par Description:
+ * Main function of the program. This function primarly passes control to the
+ * Fractal class.
  * 
  * 
  * @param[in]      argc - Number of aurments from the command line
  * @param[out]     argv - An array of command line aurgments
  * 
- * @returns 0 program ran successfully.
+ * @returns 0 - program ran successfully.
  *****************************************************************************/
 int main( int argc, char *argv[] )
 {
 	// Initialize program's core class
-	Fractal Fractal;
+	Fractal fractal;
 
 	// Run everything through the Pong class
-	return Fractal.run( argc, argv);
+	return fractal.run( argc, argv);
 }
