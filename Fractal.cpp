ad80692ec90ab29922632340d9c5c05848718a00
Deaboy <danieleandrus@gmail.com>
2014-10-20 14:27:20 -0600

    Added drawing functionality
    + Added drawing functionality to Fractal class
    + Added event implementation to Fractal
    
    Updated documentation in the following files
    - Fractal.h
    - Fractal.cpp

--- Fractal.cpp
+++ Fractal.cpp
@@ -77,3 +77,477 @@ int Fractal::run( int argc, char *argv[] )
     return 0;
 
 }
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Function to get the current instance of the program.
+ *
+ * @returns Pointer to the current instance of the program.
+*******************************************************************************/
+Fractal* Fractal::getInstance()
+{
+	return instance;
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Registers a Drawable object to be drawn on demand.
+ *
+ * @param[in]	obj - Pointer to the object to draw
+ * @param[in]	layer - OPTIONAL. The drawing layer of the object.
+ *				Greater values will cause the object to be drawn above
+ *				others. Negative values are allowed. Default is 0.
+ *				Objects at the same layer are drawn in an undefined
+ *				order.
+*******************************************************************************/
+void Fractal::drawObject(Drawable* obj, int layer)
+{
+	if (!isDrawingObject(obj))
+	{
+		drawables[layer].push_back(obj);
+	}
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Checks whether an object is currently being drawn.
+ *
+ * @param[in]	obj - Pointer to the object to search for.
+ *
+ * @returns True if the object is being drawn, false if not.
+*******************************************************************************/
+bool Fractal::isDrawingObject(Drawable* obj)
+{
+	// Loop through every later and every object in that row until we find obj
+	typedef map<int, list<Drawable*>>::iterator it_type;
+	for (it_type iterator = drawables.begin();
+		iterator != drawables.end();
+		iterator++)
+	{
+		for (Drawable* d : iterator->second)
+		{
+			if (d == obj)
+			{
+				return true;	// Found it!
+			}
+		}
+	}
+	return false;	// Didn't find it :(
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Determines the layer given the object is drawn at.
+ *
+ * @param[in]	-obj - Pointer to the object to search for.
+ *
+ * @returns The layer at which the object is being drawn. If object is
+ *		not being drawn, will return 0.
+*******************************************************************************/
+int Fractal::getDrawingLayer(Drawable* obj)
+{
+	// Loop through every later and every object in that row until we find obj
+	typedef map<int, list<Drawable*>>::iterator it_type;
+	for (it_type iterator = drawables.begin();
+		iterator != drawables.end();
+		iterator++)
+	{
+		for (Drawable* d : iterator->second)
+		{
+			if (d == obj)
+			{
+				iterator -> first;
+			}
+		}
+	}
+	return 0;	// If object is not found, return default value of 0.
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Removes an object from the drawing list and no longer draws
+ *		it. Must be done before the object becomes deallocated.
+ *
+ * @param[in]	obj - Pointer to the object to no longer draw.
+*******************************************************************************/
+void Fractal::stopDrawingObject(Drawable* obj)
+{
+	// For every later, remove reference to object. Don't bother searching.
+	typedef map<int, list<Drawable*>>::iterator it_type;
+	for (it_type iterator = drawables.begin();
+		iterator != drawables.end();
+		iterator++)
+	{
+		iterator->second.remove(obj);
+	}
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Stops drawing all objects, clearing the drawing list.
+*******************************************************************************/
+void Fractal::stopDrawingAll()
+{
+	drawables.clear();
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Gets the width of the view port in the virtual space.
+ * 
+ * @returns The width of the view port in pixels.
+*******************************************************************************/
+int Fractal::getViewWidth()
+{
+	return view_width;
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Gets the height of the view port in the virtual space.
+ *
+ * @returns The height of the view port in pixels.
+*******************************************************************************/
+int Fractal::getViewHeight()
+{
+	return view_height;
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Drawing callback. Executes every glut display callaback.
+ *		Also calls the draw function of all registerd Drawable objects.
+*******************************************************************************/
+void Fractal::display()
+{
+	//clear the display and set backround to black
+	glClear( GL_COLOR_BUFFER_BIT );
+	glColor3f( 1.0, 1.0, 1.0 );
+    
+	// Draw all registered drawables
+	typedef map<int, list<Drawable*>>::iterator it_type;
+	for (it_type iterator = drawables.begin();
+		iterator != drawables.end();
+		iterator++)
+	{
+		for (Drawable* d : iterator->second)
+		{
+			d->draw();
+		}
+	}
+
+	// Flush graphical output
+    glutSwapBuffers();
+    glFlush();
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Resize callback. Executes whenever the window is resized.
+ *
+ * @param[in]	w - The window's new width in pixels.
+ * @param[in]	h - The window's new height in pixels.
+*******************************************************************************/
+void Fractal::reshape(int w, int h)
+{
+	// store new window dimensions globally
+    window_width = w;
+    window_height = h;
+	double ratio = (double) view_width / (double) view_height;
+	double scale;
+
+    // Orthographic projection of 3-D scene onto 2-D, maintaining aspect ratio
+    glMatrixMode( GL_PROJECTION );		// Use an orthographic projection
+    glLoadIdentity();					// Initialize transformation matrix
+	if (w > h * ratio)					// Adjust viewport based on ratio
+	{
+		scale = (double) h / (double) view_height;
+		this -> scale = scale;
+		gluOrtho2D( 0 - ((w / scale) - view_width) / 2,
+			view_width + ((w / scale) - view_width) / 2,
+			0,
+			view_height );
+		
+		view_x = (w / scale - view_width) / 2;
+		view_y = 0;
+	}
+	else
+	{
+		scale = (double) w / (double) view_width;
+		this -> scale = scale;
+		gluOrtho2D( 0,
+			view_width,
+			0 - ((h / scale) - view_height) / 2,
+			view_height + ((h / scale) - view_height) / 2 );
+
+		view_x = 0;
+		view_y = (h / scale - view_height) / 2;
+	}
+    glViewport( 0, 0, w, h );			// Adjust viewport to new window
+
+    // Switch back to (default) model view mode, for transformations
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Key down callback. Executes whenever a typical key is
+ *		pressed.
+ *
+ * @param[in]	key - ASCII-encoded char of the key that was pressed.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void Fractal::keyDown(unsigned char key, int x, int y)
+{
+    // TODO
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Key up callback. Executes whenever a typical key is
+ *		released.
+ * 
+ * @param[in]	key - ASCII-encoded char of the key that was pressed.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void Fractal::keyUp(unsigned char key, int x, int y)
+{
+	// TODO
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Special key down callback. Executes whenever a "special"
+ *		key such as an arrow key is pressed.
+ *  
+ * @param[in]	key - Special GLUT enumerator of the key.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void Fractal::keySpecialDown(int key, int x, int y)
+{
+	// TODO
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Special key up callback. Executes whenever a "special" key
+ *		such as an arrow key is released.
+ *  
+ * @param[in]	key - Special GLUT enumerator of the key.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void Fractal::keySpecialUp(int key, int x, int y)
+{
+	// TODO
+}
+
+/***************************************************************************//**
+ * @author Johnny Ackerman
+ * 
+ * @par Description: Mouse click callback. Executes whenever a mouse button is
+ *		either clicked or released.
+ *  
+ * @param[in]	button - The button whose state was changed.
+ * @param[in]	state - The new state of the button.
+ * @param[in]	x - The x coordinate of the mouse at the time the button
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the button
+ *				was pressed.
+*******************************************************************************/
+void Fractal::mouseclick(int button, int state, int x, int y)
+{
+	// Correct for coordinate system
+    y = window_height - y;
+
+	// Correct for scaling
+	x /= scale;
+	y /= scale;
+
+	x -= view_x;
+	y -= view_y;
+
+    // TODO
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Step callback. Called every frame of the game. Calls step
+ *		function of other classes necessary for the game. Necessary
+ *		for game elemnts that are not dependent on user interaction.
+*******************************************************************************/
+void Fractal::step()
+{
+	// TODO
+}
+
+/*******************************************************************************
+ *                         GLUT CALLBACK FUNCTIONS
+*******************************************************************************/
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Drawing callback. Executes every glut display callaback.
+ *		Also calls the draw function of all registerd Drawable objects. Simply
+ *		forwards to Pong class' identical function.
+*******************************************************************************/
+void display()
+{
+	Fractal::getInstance()->display();
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Resize callback. Executes whenever the window is resized.
+ *		Simply forwards to Pong class' identical function.
+ *
+ * @param[in]	w - The window's new width in pixels.
+ * @param[in]	h - The window's new height in pixels.
+*******************************************************************************/
+void reshape(int w, int h)
+{
+	Fractal::getInstance()->reshape(w, h);
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Key down callback. Executes whenever a typical key is
+ *		pressed. Simply forwards to Pong class' identical function.
+ *
+ * @param[in]	key - ASCII-encoded char of the key that was pressed.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void keyDown(unsigned char key, int x, int y)
+{
+	Fractal::getInstance()->keyDown(key, x, y);
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Key up callback. Executes whenever a typical key is
+ *		released. Simply forwards to Pong class' identical function.
+ * 
+ * @param[in]	key - ASCII-encoded char of the key that was pressed.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void keyUp(unsigned char key, int x, int y)
+{
+	Fractal::getInstance()->keyUp(key, x, y);
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Special key down callback. Executes whenever a "special"
+ *		key such as an arrow key is pressed. Simply forwards to Pong class'
+ *		identical function.
+ *  
+ * @param[in]	key - Special GLUT enumerator of the key.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void keySpecialDown(int key, int x, int y)
+{
+	Fractal::getInstance()->keySpecialDown(key, x, y);
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus, Johnny Ackerman
+ * 
+ * @par Description: Special key up callback. Executes whenever a "special" key
+ * 		such as an arrow key is released. Simply forwards to Pong class'
+ *		identical function.
+ *  
+ * @param[in]	key - Special GLUT enumerator of the key.
+ * @param[in]	x - The x coordinate of the mouse at the time the key
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the key
+ *				was pressed.
+*******************************************************************************/
+void keySpecialUp(int key, int x, int y)
+{
+	Fractal::getInstance()->keySpecialUp(key, x, y);
+}
+
+/***************************************************************************//**
+ * @author Johnny Ackerman
+ * 
+ * @par Description: Mouse click callback. Executes whenever a mouse button is
+ * 		either clicked or released. Simply forwards to Pong class'
+ * 		identical function.
+ *  
+ * @param[in]	button - The button whose state was changed.
+ * @param[in]	state - The new state of the button.
+ * @param[in]	x - The x coordinate of the mouse at the time the button
+ *				was pressed. Measured in integers.
+ * @param[in]	y - The y coordinate of the mouse at the time the button
+ *				was pressed.
+*******************************************************************************/
+void mouseclick(int button, int state, int x, int y)
+{
+	Fractal::getInstance()->mouseclick(button, state, x, y);
+}
+
+/***************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Step callback. Called every frame of the game. Calls step
+ *		function of other classes necessary for the game. Necessary
+ *		for game elemnts that are not dependent on user interaction.
+ *		Simply to Pong class' step() function.
+ *
+ * @param[in]	i - Necessary for GLUT. Unused.
+*******************************************************************************/
+void step(int i)
+{
+	// FPS, or technically "milliseconds per frame"
+	static unsigned int fps_delay = 1000 / 60;
+	
+	// Reset the timer
+	glutTimerFunc(fps_delay, *::step, 0);
+	
+	// Call step function
+	Fractal::getInstance()->step();
+
+	// Redraw the screen after frame's been processed.
+	glutPostRedisplay();
+}
+
--- Fractal.h
+++ Fractal.h
@@ -22,18 +22,81 @@ class Fractal;
 
 using namespace std;
 
-
+/***************************************************************************//**
+ * @brief The Fractal class is in charge of window management, drawing
+ *		all the parts of the screen, and running the other classes. It also
+ *		handles the interaction between glut and the program and event
+ *		management.
+ *
+ * @details This class essentially provides a base for the rest of the program.
+ *		It handles drawing functions, forwards OpenGL events, generates a
+ *		framerate, and standardizes various aspects of the program.
+*******************************************************************************/
 class Fractal
 {
+	private:
+		static Fractal* instance;	/*!< reference to main instance */
+		int view_x;					/*!< The x offset of the drawing region */
+		int view_y;					/*!< The y offset of the drawing region */
+		int view_width;				/*!< The width of the drawable region */
+		int view_height;			/*!< The height of the drawable region */
+		int window_width;			/*!< Width of gui window */
+		int window_height;			/*!< Height of gui window */
+		double scale;				/*!< Scale of the gui contents */
+		string window_name;			/*!< Name of the gui window */
+		map<int, list<Drawable*>> drawables;	/*!< list of objects to draw*/
+
 	public:
 		Fractal();
 		~Fractal();
 		int run( int argc, char *argv[] );
 
 
-/*******************************************************************************
- *                 GLUT CALLBACK FUNCTIONS FROM THE CLASS
-*******************************************************************************/
+		/*!
+		 * @brief Function to get the current instance of the program.
+		 */
+		static Fractal* getInstance( );
+
+		/*!
+		 * @brief Registers a Drawable object to be drawn on demand.
+		 */
+		void drawObject(Drawable* obj, int layer = 0);
+		
+		/*!
+		 * @brief Checks whether an object is currently being drawn.
+		 */
+		bool isDrawingObject(Drawable* obj);
+
+		/*!
+		 * @brief Determines the layer given the object is drawn at.
+		 */
+		int getDrawingLayer(Drawable* obj);
+
+		/*!
+		 * @brief Sets the drawing layer of an already registered object.
+		 */
+		void setDrawingLayer(Drawable* obj, int layer);
+
+		/*!
+		 * @brief Removes an object from the drawing list and no longer draws
+		 *		it. Must be done before the object becomes deallocated.
+		 */
+		void stopDrawingObject(Drawable* obj);
+
+		/*!
+		 * @brief Stops drawing all objects, clearing the drawing list.
+		 */
+		void stopDrawingAll();
+
+		/*!
+		 * @brief Gets the width of the view port in the virtual space.
+		 */
+		int getViewWidth();
+
+		/*!
+		 * @brief Gets the height of the view port in the virtual space.
+		 */
+		int getViewHeight();
 
 		/*!
 		 * @brief Drawing callback. Executes every glut display callaback. Also
