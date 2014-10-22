c3392b06c4eccfd14e1441f19aab188b77f7bc8e
Deaboy <danieleandrus@gmail.com>
2014-10-21 18:52:26 -0600

    Updated documentation
    Added documentation to the following files:
    - ViewManager.h
    - ViewManager.cpp
    - View.cpp

--- ViewManager.cpp
+++ ViewManager.cpp
@@ -1,5 +1,28 @@
+/***************************************************************************//**
+ * @file File containing the implementation for the ViewManager class.
+ *
+ * @brief Contains the implementation for the ViewManager class.
+*******************************************************************************/
+/*******************************************************************************
+ *                 DECLARATIONS, INCLUDES, AND NAMESPACES
+*******************************************************************************/
 #include "ViewManager.h"
 
+/*******************************************************************************
+ *                          FUNCTION DEFINITIONS
+*******************************************************************************/
+/**************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Registers a view under the given name. If a view under the
+ *      given name already exists, will fail and return false.
+ *
+ * @param[in]	name - The name to register the view under.
+ * @param[in]	view - Pointer to the view to register. Fails if pointer is
+ *      NULL.
+ *
+ * @returns true if the view was successfully registered, false if not.
+ *****************************************************************************/
 bool ViewManager::registerView(string name, View* view)
 {
 	if (view == NULL) return false;
@@ -12,14 +35,29 @@ bool ViewManager::registerView(string name, View* view)
 	return false;
 }
 
+/**************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Unregisteres the view that is registered under the given
+ *      name. If the target view is currently the active view, stops drawing
+ *      that view.
+ *
+ * @param[in]	name - The name of the view to unregister.
+ *
+ * @returns true if successful, false if not.
+ *****************************************************************************/
 bool ViewManager::unregisterView(string name)
 {
-	View* view = getRegisteredView(name);
-	if (view != NULL)
+	// Make sure the view exists before attempting to unregister it
+	View* v = getRegisteredView(name);
+	if (v != NULL)
 	{
-		view -> hide();
+		// Hide and erase the view
+		v -> hide();
 		views.erase(name);
-		if (active_view == view)
+		
+		// Take proper action if the target view is the currently active view
+		if (active_view == v)
 		{
 			active_view = NULL;
 		}
@@ -28,14 +66,28 @@ bool ViewManager::unregisterView(string name)
 	return false;
 }
 
+/**************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Hides the currently active view and makes the view
+ *      registered under the given name visible.
+ *
+ * @param[in]	name - The name under which the desired view is registered.
+ *
+ * @returns true if the switch was successful, false if not.
+ *****************************************************************************/
 bool ViewManager::switchView(string name)
 {
+	// First, make sure the target is even registered
 	if (getRegisteredView(name) != NULL)
 	{
+		// Hide the currently active view if it exists
 		if (active_view != NULL)
 		{
 			active_view -> hide();
 		}
+		
+		// Make the desired view active and show it
 		active_view = getRegisteredView(name);
 		active_view -> show();
 		return true;
@@ -43,8 +95,20 @@ bool ViewManager::switchView(string name)
 	return false;
 }
 
+/**************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Gets a pointer to the view registered under the given
+ *      name.
+ *
+ * @param[in]	name - The name under which the desired view is registered.
+ *
+ * @returns Pointer to the view registered under the given name, or NULL if no
+ *      views are registered under that name.
+ *****************************************************************************/
 View* ViewManager::getRegisteredView(string name)
 {
+	// Use a try-catch to see if the item is in the map. If not, return NULL.
 	try
 	{
 		return views.at(name);
@@ -56,7 +120,30 @@ View* ViewManager::getRegisteredView(string name)
 	}
 }
 
+/**************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Gets a pointer to the currently active view.
+ *
+ * @returns Pointer to the currently active view, or NULL if no views are
+ *      active.
+ *****************************************************************************/
 View* ViewManager::getActiveView()
 {
 	return active_view;
+}
+
+/**************************************************************************//**
+ * @author Daniel Andrus
+ * 
+ * @par Description: Draws the currently active view or nothing if there are
+ *      no active views.
+ *****************************************************************************/
+void ViewManager::draw()
+{
+	// Of course, make sure we have an active view before attempting to draw it
+	if (active_view != NULL)
+	{
+		active_view -> draw();
+	}
 }
--- ViewManager.h
+++ ViewManager.h
@@ -1,26 +1,69 @@
+/***************************************************************************//**
+ * @file File containing the declaration for the ViewManager class.
+ *
+ * @brief Contains the declaration for the ViewManager class.
+*******************************************************************************/
 #ifndef _VIEWMANAGER_H_
 #define _VIEWMANAGER_H_
 
+/*******************************************************************************
+ *                 DECLARATIONS, INCLUDES, AND NAMESPACES
+*******************************************************************************/
 class ViewManager;
 
 #include <map>
 #include <string>
 #include "View.h"
+#include "Drawable.h"
 
 using namespace std;
 
-class ViewManager
+/*******************************************************************************
+ *                              CLASS INTERFACE
+ ******************************************************************************/
+/*!
+ * @brief Interface for a ViewManager class. Declares methods for registering
+ *      and managing multiple views, including switching between views.
+ *
+ * @details Provides functions and data for managing multiple views.
+ */
+class ViewManager : Drawable
 {
 private:
-	map<string, View*> views;
-	View* active_view;
+	map<string, View*> views;	/*!< Map of registered views */
+	View* active_view;			/*!< Pointer to currently active view */
 	
 public:
+	
+	/*!
+	 * @brief Registers a view
+	 */
 	bool registerView(string name, View* view);
+	
+	/*!
+	 * @brief Unregisteres a view from the manager
+	 */
 	bool unregisterView(string name);
+	
+	/*!
+	 * @brief Hides the current view and displays the given view
+	 */
 	bool switchView(string name);
+	
+	/*!
+	 * @brief Gets a pointer to the view registered under the given name
+	 */
 	View* getRegisteredView(string name);
+	
+	/*!
+	 * @brief Gets a pointer to the currently active view
+	 */
 	View* getActiveView();
+	
+	/*!
+	 * @brief Draws the currently active view if one is active
+	 */
+	void draw();
 };
 
 #endif
